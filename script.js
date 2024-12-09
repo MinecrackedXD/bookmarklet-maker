b74b2d43ac113515c1c4ac92ee2475b6c3bc26a6
MinecrackedXD <gioisstilltalking@gmail.com>
2024-12-10 00:28:47 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-document.getElementById('copyButton').addEventListener('click', function() {
-    const bookmarkletCode = document.getElementById('bookmarkletCode');
-    bookmarkletCode.select();
+function generateBookmarklet() {
+    const inputBox = document.getElementById('inputBox');
+    const outputBox = document.getElementById('outputBox');
+    const copyButton = document.getElementById('copyButton');
+
+    const code = inputBox.value.trim();
+
+    if (code) {
+        const bookmarkletCode = `javascript:(${code})();`;
+        outputBox.value = bookmarkletCode;
+        copyButton.style.display = 'inline-block';
+    } else {
+        outputBox.value = '';
+        copyButton.style.display = 'none';
+    }
+}
+
+function copyCode() {
+    const outputBox = document.getElementById('outputBox');
+    outputBox.select();
     document.execCommand('copy');
     alert('Code copied to clipboard!');
-});
+}
